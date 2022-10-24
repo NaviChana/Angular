e2234e586317535aa703a4568239680d996865bc
NaviChana <NaviChana@outlook.com>
2022-10-23 21:23:13 -0600

    Added E-Commerce items;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CartComponent } from './components/cart/cart.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
-  {path:'login', component: LoginComponent},
-  {path:'signup', component: SignupComponent}
+  {path:'login'     ,   component: LoginComponent},
+  {path:'signup'    ,   component: SignupComponent},
+  {path:'header'    ,   component: HeaderComponent},
+  {path:'products'  ,   component: ProductsComponent},
+  {path:'cart'      ,   component: CartComponent},
+  {path: '**'       ,   component:HomeComponent}
 ];
 
 @NgModule({
@@ -14,4 +22,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [LoginComponent, SignupComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,24 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
 import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents,
+    LoginComponent,
+    SignupComponent,
     HeaderComponent,
+    ProductsComponent,
     CartComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
